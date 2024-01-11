99873b50eb2990c594fd3ed843bd51796da3b8af
ATHUL KRISHNA MUTHANDASSERY SOMAN 2200145G <2200145G@student.tp.edu.sg>
2024-01-11 15:03:07 +0800

    configured register,login and logout

--- CrystalMeds/Server/Models/ApplicationUser.cs
+++ CrystalMeds/Server/Models/ApplicationUser.cs
@@ -4,5 +4,7 @@ namespace CrystalMeds.Server.Models
 {
     public class ApplicationUser : IdentityUser
     {
-    }
+		public string? FirstName { get; set; }
+		public string? LastName { get; set; }
+	}
 }
